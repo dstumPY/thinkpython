313715b04741f9c2abb69707ad41861329b8bd02
dstumPY <dannystumpe@gmail.com>
2020-04-23 23:04:00 +0200

    Added ex 12.3

--- 12_tuples.py
+++ 12_tuples.py
@@ -1,5 +1,6 @@
-from typing import Dict
+from typing import Dict, List
 from pprint import pprint
+import pickle
 
 
 def exercise_12_1():
@@ -10,20 +11,27 @@ def exercise_12_1():
 def exercise_12_2():
 
     d = {}
-    with open('words.txt', 'r') as file:
+    with open("words.txt", "r") as file:
         for line in file:
             word = line.strip()
             k = to_alphabetical(word)
             if k not in d:
-                d[k] = {'anagram': [word]}
+                d[k] = {"anagram": [word]}
             else:
-                d[k]['anagram'].append(word)
+                d[k]["anagram"].append(word)
 
     for _, val in d.items():
-        val['len'] = len(val['anagram'])
-    
-    tmp_dict = sorted(d.items(), key=lambda item: item[1]['len'], reverse=True)
-    pprint(tmp_dict)
+        val["len"] = len(val["anagram"])
+
+    result_dict = sorted(d.items(), key=lambda item: item[1]["len"], reverse=True)
+
+    with open("anagram_dict.pkl", "wb") as pickle_dumper:
+        pickle.dump(result_dict, pickle_dumper)
+    # # 12.2.c)
+    # for item in result_dict:
+    #     if len(item[0]) == 8:
+    #         print(item)
+    #         break
 
 
 def hist(str1: str) -> Dict:
@@ -43,10 +51,60 @@ def most_frequent(str1: str) -> Dict:
 def is_anagram(str1: str, str2: str):
     return most_frequent(str1) == most_frequent(str2)
 
+
 def to_alphabetical(str1: str) -> str:
     sorted_ls = sorted(str1)
-    return ''.join(sorted_ls)
+    return "".join(sorted_ls)
+
+
+def read_anagram():
+    with open("anagram_dict.pkl", "rb") as pickle_loader:
+        anagram_dict = pickle.load(pickle_loader)
+    return anagram_dict
+
+
+def get_permutations(str_len: int):
+    ls = [(i, j) for i in range(str_len) for j in range(str_len) if i < j]
+    return ls
+
+
+def switch_by_perm(str1: str, perm: List[int]) -> List[str]:
+    init_ls = list(str1)
+    tmp_var = init_ls[perm[0]]
+    init_ls[perm[0]] = init_ls[perm[1]]
+    init_ls[perm[1]] = tmp_var
+    return init_ls
+
+
+def is_meta_pair(str1: str, str2: str):
+    tmp_len = len(str1)
+    assert tmp_len == len(str2), "Not same length"
+    permutation_ls = get_permutations(tmp_len)
+    for perm in permutation_ls:
+        test_str1 = switch_by_perm(str1, perm)
+        test_str2 = list(str2)
+        if test_str1 == test_str2:
+            return True
+        else:
+            continue
+    return False
+
+
+def exercise_12_3():
+    anagram_ls = read_anagram()
+    # modify dict to sort out items with no matching anagrams
+    anagram_ls = [item[1] for item in anagram_ls if item[1]["len"] > 2]
+    for anagram_item in anagram_ls:
+        working_list = anagram_item["anagram"]
+        for index, word in enumerate(working_list):
+            for test_word in working_list[index:]:
+                if is_meta_pair(word, test_word) and (word != test_word):
+                    print(word, test_word)
+                else:
+                    continue
 
 
 if __name__ == "__main__":
-    exercise_12_2()
+    # exercise_12_2()
+    exercise_12_3()
+    # print(is_meta_pair("blas", "bald"))
