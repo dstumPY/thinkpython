29c94c22b23bedb47a50de5ed76f032115b50353
danny.stumpe.fp <danny.stumpe.fp@outlook.de>
2020-04-23 09:16:30 +0200

    Improved performance on ex 12_2

--- 12_tuples.py
+++ 12_tuples.py
@@ -1,34 +1,6 @@
 from typing import Dict
 from pprint import pprint
 
-ALPHABET = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-]
 
 def exercise_12_1():
     test_str = "parrot"
@@ -36,47 +8,23 @@ def exercise_12_1():
 
 
 def exercise_12_2():
-    # transform a list of words similar to test list into a dict which 
-    # has the letters from ALPHABET as keys and the words containing 
-    # that special keys as list assigned ({'a': ["parrot", "oawi", ..], "b": ..})
-    test_list = ["parrot", "asugba", "torpar", "dbs", "wof", "oawi", "sdb", "bds"]
-    lexic = {letter: [] for letter in ALPHABET}
-    for letter, val in lexic.items():
-        for word in test_list:
-            if letter in word:
-                val.append(word)
-    pprint(lexic)
 
-    result_dict = {}
+    d = {}
+    with open('words.txt', 'r') as file:
+        for line in file:
+            word = line.strip()
+            k = to_alphabetical(word)
+            if k not in d:
+                d[k] = {'anagram': [word]}
+            else:
+                d[k]['anagram'].append(word)
 
-    # loop over the keys from lexic and check only the word list
-    # assigned to key for an anagram. Since anagrams must have the
-    # same length it's sufficient to check a word only to a sublist 
-    # of words with the same length
-    for letter, letter_list in lexic.items():
-        # loop over specific letter list
-        # (= list corresponding to one letter)
-        for word in letter_list:
-            # filter only for words with the same length
-            tmp_list = [
-                item_word
-                for item_word in letter_list
-                if (len(item_word) == len(word)) and item_word != word
-            ]
+    for _, val in d.items():
+        val['len'] = len(val['anagram'])
+    
+    tmp_dict = sorted(d.items(), key=lambda item: item[1]['len'], reverse=True)
+    pprint(tmp_dict)
 
-            # Loop over the words with same length to test is_anagram.
-            # If an anagram is found we normalize the word to a string
-            # with chars in alphabetical order. The normalized string
-            # builds the keys for the found anagram words that will 
-            # be assigned as list
-            for eq_len_word in tmp_list:
-                if is_anagram(eq_len_word, word):
-                    key_str = to_alphabetical(word)
-                    val_set = result_dict.setdefault(key_str, set())
-                    val_set.add(eq_len_word)
-                    #print(result_dict[key_str])
-                    #print(letter, word, eq_len_word)
-    pprint(result_dict)
 
 def hist(str1: str) -> Dict:
     hist_dic = {}
